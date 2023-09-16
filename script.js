7a57a927095f27961006784d611cc1b82fcae8fb
Harika Ravula <144255522+harirv9@users.noreply.github.com>
2023-09-16 17:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+
+      
+    
+    //printing output in java script
+      console.log("hello");
+
+     // single line comment // is used
+     /* this is used for multiline comments */
+
+     //variables declaration
+     let a=5;
+     let b=10;
+     //a,b are the variables and 5,10 are the values
+     console.log(a);
+     console.log(b);
+     //example1 for variables
+     let num1=10;
+     let num2=100;
+     let num3=num1+num2;
+     console.log(num3);
+     //example2 for variables
+     let numOfCubes=50;
+     let num_of_people=10;
+     console.log(numOfCubes);
+     console.log(num_of_people);
+     //constants declaration
+     const c=30;
+     console.log(c);
+     //operators in js
+
+     //arithematic operators
+     let n1=200;
+     let n2=500;
+     console.log(n1+n2);
+     console.log(b-a);
+     console.log(num1*num2);
+     console.log(n1/n2);
+     console.log(a%b);
+     console.log(b**a);
+
+     //assignment operators
+     let n3=10;
+     let n4=10;//assigning a value "=" is used
+     console.log(n3+=5);
+     console.log(n3-=6);
+     console.log(n4*=3);
+     console.log(n4/=5);
+     console.log(n3**=2);
+
+     //comparision operators
+     let app1=10;
+     let app2=50;
+     let app3=10;
+     console.log(app1==app2);
+     console.log(app1==app3);
+     console.log(app1!=app2);
+     console.log(app1>app2);
+     console.log(app2<app2);
+     console.log(app3<=app2);
+     console.log(app2>=app3);
+
+     //data types
+     let number=12;//number type
+     let alphabet="abc";//string type
+     let apple=true;//boolean
+     console.log(alphabet);
+     console.log(apple);
+     //==,=== differences
+     let number1="12";
+     console.log(number==number1);
+     console.log(number===number1);
+     
+     //strings
+     let firstName="harika"
+     let secondName="ravula"
+     console.log(firstName+secondName);
+     //concat
+     console.log(firstName.concat(secondName));//concat
+     //spaces
+     console.log(firstName+""+secondName);//space between two strings
+     console.log(firstName+"  "+secondName);
+     //uppercase
+     console.log(firstName.toUpperCase());
+     //lowercase
+     console.log(secondName.toLowerCase());
+     //length
+     console.log(firstName.length);
+     console.log(firstName.length+secondName.length);
+     //replace
+     console.log(firstName.replace("harika","hari"));//replace firstName with other name
+     //slice
+     console.log(firstName.slice(1,5));//extract certain part
+     //subString
+     console.log(secondName.substring(1,4));
+     //split
+     let str1="what are you doing";
+     console.log(str1.split(" ",3));//split method 
+     //charAt
+     console.log(str1.charAt(7));//charAt method to get letter present in 7th position of str1
+     //trim, trimStart
+     let str2="     hello all   "
+     console.log(str2.trim());//trim method to remove white spaces
+     console.log(str2.trimStart());
+     //padstart, padend
+     let text="4"
+     console.log(text.padStart(3,"0"));
+     console.log(text.padEnd(3,"0"));
+     //replace
+     let str3="hello world";
+     console.log(str3.replace("world","guys"));
+     //replaceAll
+     let str4="strings contains many methods. All methods have are used in various ways";
+     console.log(str4.replaceAll("methods", "METHODS"));
+     //charcodeAt
+     let str5="happy birthday";
+     console.log(str5.charCodeAt(0));
+
+
+
+
+
+
+
+     
+
+
+     
+
+
+    
+
